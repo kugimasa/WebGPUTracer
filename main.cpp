706c09640e25562e10e1455e46156d2c2b569b79
kugimasa <kugimania@gmail.com>
2023-06-02 01:11:55 +0900

    WebGPU: Render first color through render pass

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #endif
 
 int main() {
-  std::cout << "Starting Portracer (_)=---=(_)" << std::endl;
+  Print(PrintInfoType::Portracer, "Starting Portracer (_)=---=(_)");
 
   /// Initialize GLFW
   if (!glfwInit()) {
@@ -20,6 +20,7 @@ int main() {
   }
   /// Create Window
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
   GLFWwindow *window = glfwCreateWindow(640, 480, "Portracer (_)=---=(_)", NULL, NULL);
 
   /// Setup WebGPU
@@ -42,7 +43,6 @@ int main() {
   adapter_options.compatibleSurface = surface;
   WGPUAdapter adapter = RequestAdapter(instance, &adapter_options);
   Print(PrintInfoType::WebGPU, "Got adapter:", adapter);
-  ShowAdapterFeature(adapter);
 
   /// Get WebGPU device
   Print(PrintInfoType::WebGPU, "Requesting device ...");
@@ -62,28 +62,27 @@ int main() {
   /// Get device queue
   WGPUQueue queue = wgpuDeviceGetQueue(device);
   #ifdef WEBGPU_BACKEND_DAWN
-  // FIXME: signalValue is unknown
+  // signalValue is 0 for now
   wgpuQueueOnSubmittedWorkDone(queue, 0, OnQueueWorkDone, nullptr);
   #endif
 
-  /// Command encoder
-  WGPUCommandEncoderDescriptor encoder_desc = {};
-  encoder_desc.nextInChain = nullptr;
-  encoder_desc.label = "Portracer Command Encoder";
-  WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoder_desc);
-  // Debug placeholder for now
-  wgpuCommandEncoderInsertDebugMarker(encoder, "Insert Debug Marker");
-  wgpuCommandEncoderInsertDebugMarker(encoder, "Portracer running ...");
-
-  /// Create command buffer from command encoder
-  WGPUCommandBufferDescriptor cmd_buffer_desc = {};
-  cmd_buffer_desc.nextInChain = nullptr;
-  cmd_buffer_desc.label = "Command Buffer";
-  WGPUCommandBuffer command = wgpuCommandEncoderFinish(encoder, &cmd_buffer_desc);
-
-  /// Submit command queue
-  Print(PrintInfoType::WebGPU, "Submitting command ...");
-  wgpuQueueSubmit(queue, 1, &command);
+  /// Create swap chain
+  WGPUSwapChainDescriptor swap_chain_desc = {};
+  swap_chain_desc.nextInChain = nullptr;
+  swap_chain_desc.width = 640;
+  swap_chain_desc.height = 480;
+  /// Texture format
+  #ifdef WEBGPU_BACKEND_WGPU
+  WGPUTextureFormat swap_chain_format = wgpuSurfaceGetPreferredFormat(surface, adapter);
+  swap_chain_desc.format = swap_chain_format;
+  #else
+  // For Dawn BGRA8Unorm only
+  swap_chain_desc.format = WGPUTextureFormat_BGRA8Unorm;
+  #endif
+  swap_chain_desc.usage = WGPUTextureUsage_RenderAttachment;
+  swap_chain_desc.presentMode = WGPUPresentMode_Fifo;
+  WGPUSwapChain swap_chain = wgpuDeviceCreateSwapChain(device, surface, &swap_chain_desc);
+  Print(PrintInfoType::WebGPU, "Swapchain: ", swap_chain);
 
   /// Use Window
   if (!window) {
@@ -92,12 +91,59 @@ int main() {
     return 1;
   }
   while (!glfwWindowShouldClose(window)) {
-    // Check whether the user clicked on the close button (and any other
-    // mouse/key event, which we don't use so far)
     glfwPollEvents();
+
+    // Get target texture view
+    WGPUTextureView next_texture = wgpuSwapChainGetCurrentTextureView(swap_chain);
+    if (!next_texture) {
+      Error(PrintInfoType::WebGPU, "Cannot acquire next swap chain texture");
+      break;
+    }
+    Print(PrintInfoType::WebGPU, "next_texture: ", next_texture);
+
+    // Draw
+    /// Command encoder
+    WGPUCommandEncoderDescriptor encoder_desc = {};
+    encoder_desc.nextInChain = nullptr;
+    encoder_desc.label = "Portracer Command Encoder";
+    WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoder_desc);
+    /// Create Render pass
+    WGPURenderPassDescriptor render_pass_desc = {};
+    WGPURenderPassColorAttachment render_pass_color_attachment = {};
+    // Set the texture view directly
+    render_pass_color_attachment.view = next_texture;
+    // This is for multi-sampling
+    render_pass_color_attachment.resolveTarget = nullptr;
+    render_pass_color_attachment.loadOp = WGPULoadOp_Clear;
+    render_pass_color_attachment.storeOp = WGPUStoreOp_Store;
+    render_pass_color_attachment.clearValue = WGPUColor{0.274f, 0.886f, 0.745f, 1.0f};
+    render_pass_desc.colorAttachmentCount = 1;
+    render_pass_desc.colorAttachments = &render_pass_color_attachment;
+    render_pass_desc.depthStencilAttachment = nullptr;
+    render_pass_desc.timestampWriteCount = 0;
+    render_pass_desc.timestampWrites = nullptr;
+    render_pass_desc.nextInChain = nullptr;
+    WGPURenderPassEncoder render_pass = wgpuCommandEncoderBeginRenderPass(encoder, &render_pass_desc);
+    /// Just end the command for now
+    wgpuRenderPassEncoderEnd(render_pass);
+
+    // Destroy texture view
+    wgpuTextureViewRelease(next_texture);
+
+    /// Submit command queue
+    WGPUCommandBufferDescriptor cmd_buffer_desc = {};
+    cmd_buffer_desc.nextInChain = nullptr;
+    cmd_buffer_desc.label = "Command buffer";
+    WGPUCommandBuffer command = wgpuCommandEncoderFinish(encoder, &cmd_buffer_desc);
+    wgpuQueueSubmit(queue, 1, &command);
+
+    // Present texture
+    wgpuSwapChainPresent(swap_chain);
   }
 
   /// WebGPU stuff
+  // Release WebGPU swap chain
+  wgpuSwapChainRelease(swap_chain);
   // Release WebGPU device
   wgpuDeviceRelease(device);
   // Release WebGPU surface
