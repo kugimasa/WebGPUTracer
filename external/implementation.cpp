b3e4571140481899dad61c5788266c92382116f7
kugimasa <kugimania@gmail.com>
2023-07-04 02:10:11 +0900

    Add submodules stb, tinyobjloader

--- external/implementation.cpp
+++ external/implementation.cpp
@@ -1,2 +1,22 @@
 #define WEBGPU_CPP_IMPLEMENTATION
-#include "webgpu/webgpu.hpp"
+#include <webgpu/webgpu.hpp>
+
+// Disable pedantic warnings for this external library.
+#ifdef _MSC_VER
+// Microsoft Visual C++ Compiler
+#pragma warning (push, 0)
+#endif
+
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+// Restore warning levels.
+#ifdef _MSC_VER
+// Microsoft Visual C++ Compiler
+#pragma warning (pop)
+#endif
