a5f696e55073c0943632c61e8f8ea63d1bbd3ad2
kugimasa <kugimania@gmail.com>
2023-08-19 13:07:11 +0900

    Implement translation of CornellBox.

--- src/objects/cornell_box.cpp
+++ src/objects/cornell_box.cpp
@@ -10,27 +10,27 @@ CornellBox::CornellBox(Point3 center, Vec3 scale) {
   auto s_y = scale_.Y() / 2.0f;
   auto s_z = scale_.Z() / 2.0f;
   /// Bottom
-  auto pos = Vec3(0, -s_y, p_z);
+  auto pos = Vec3(p_x, p_y - s_y, p_z);
   auto right = Vec3(s_x, 0, 0);
   auto up = Vec3(0, 0, -s_z);
   quads_.emplace_back(pos, right, up, Color3(0, 0, 0));
   /// Top
-  pos = Vec3(0, s_y, p_z);
+  pos = Vec3(p_x, p_y + s_y, p_z);
   right = Vec3(s_x, 0, 0);
   up = Vec3(0, 0, s_z);
   quads_.emplace_back(pos, right, up, Color3(0, 0, 0));
   /// Right
-  pos = Vec3(s_x, 0, p_z);
+  pos = Vec3(p_x + s_x, p_y, p_z);
   right = Vec3(0, 0, s_z);
   up = Vec3(0, s_y, 0);
   quads_.emplace_back(pos, right, up, Color3(0, 0, 0));
   /// Left
-  pos = Vec3(-s_x, 0, p_z);
+  pos = Vec3(p_x - s_x, p_y, p_z);
   right = Vec3(0, 0, -s_z);
   up = Vec3(0, s_y, 0);
   quads_.emplace_back(pos, right, up, Color3(0, 0, 0));
   /// Far
-  pos = Vec3(0, 0, p_z - s_z);
+  pos = Vec3(p_x, p_y, p_z - s_z);
   right = Vec3(s_x, 0, 0);
   up = Vec3(0, s_y, 0);
   quads_.emplace_back(pos, right, up, Color3(0, 0, 0));
