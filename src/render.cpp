ecbf96661d771e748e613e98874e2679dba0eb95
kugimasa <kugimania@gmail.com>
2023-08-24 00:29:20 +0900

    Comment out saveTexture for test.

--- src/render.cpp
+++ src/render.cpp
@@ -417,11 +417,11 @@ bool Renderer::OnRender(int frame) {
   /// PNG出力
   std::ostringstream sout;
   sout << std::setw(3) << std::setfill('0') << frame;
-  std::string output_file = OUTPUT_DIR "/" + sout.str() + ".png";
-  if (!saveTexture(output_file.c_str(), device_, texture_, 0 /* output MIP level */)) {
-    Error(PrintInfoType::Portracer, "Image output failed.");
-    return false;
-  }
+//  std::string output_file = OUTPUT_DIR "/" + sout.str() + ".png";
+//  if (!saveTexture(output_file.c_str(), device_, texture_, 0 /* output MIP level */)) {
+//    Error(PrintInfoType::Portracer, "Image output failed.");
+//    return false;
+//  }
   // Clean up
   commands.release();
   encoder.release();
